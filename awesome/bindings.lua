925cc0aed6d0689b1da06e7919f013c0e84039f5
Mikhail Pletnev <mmp.dux@gmail.com>
2022-09-30 02:18:43 +0300

    pretty up bindings table

--- awesome/bindings.lua
+++ awesome/bindings.lua
@@ -66,53 +66,57 @@ function f_lua_prompt()
     }
 end
 
+function f_spawn(cmd)
+    return function() awful.spawn(cmd) end
+end
+
 -- {{{ Key bindings
 awful.keyboard.append_global_keybindings({
-    awful.key({ modkey }, "s", hotkeys_popup.show_help, {description="show help", group="awesome"}),
-    awful.key({ modkey }, "Left",   awful.tag.viewprev, {description = "view previous", group = "tag"}),
-    awful.key({ modkey }, "Right",  awful.tag.viewnext, {description = "view next", group = "tag"}),
-    awful.key({ modkey }, "Escape", awful.tag.history.restore, {description = "go back", group = "tag"}),
-    awful.key({ modkey }, "j", function () awful.client.focus.byidx( 1) end, {description = "focus next by index", group = "client"}),
-    awful.key({ modkey }, "k", function () awful.client.focus.byidx(-1) end, {description = "focus previous by index", group = "client"}),
+    awful.key({ modkey }, "s"                , hotkeys_popup.show_help                              , {description="show help", group="awesome"}),
+    --awful.key({ modkey }, "Left"             , awful.tag.viewprev                                   , {description = "view previous", group = "tag"}),
+    --awful.key({ modkey }, "Right"            , awful.tag.viewnext                                   , {description = "view next", group = "tag"}),
+    awful.key({ modkey }, "Escape"           , awful.tag.history.restore                            , {description = "go back", group = "tag"}),
+    awful.key({ modkey }, "j"                , function () awful.client.focus.byidx( 1) end         , {description = "focus next by index", group = "client"}),
+    awful.key({ modkey }, "k"                , function () awful.client.focus.byidx(-1) end         , {description = "focus previous by index", group = "client"}),
 
     -- Layout manipulation
     --
-    awful.key({ modkey, "Shift"   }, "j"     , function () awful.client.swap.byidx(  1)    end                      , {description = "swap with next client by index", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "k"     , function () awful.client.swap.byidx( -1)    end                      , {description = "swap with previous client by index", group = "client"}),
-    awful.key({ modkey, "Control" }, "j"     , function () awful.screen.focus_relative( 1) end                      , {description = "focus the next screen", group = "screen"}),
-    awful.key({ modkey, "Control" }, "k"     , function () awful.screen.focus_relative(-1) end                      , {description = "focus the previous screen", group = "screen"}),
-    awful.key({ modkey,           }, "u"     , awful.client.urgent.jumpto                                           , {description = "jump to urgent client", group = "client"}),
+    awful.key({ modkey, "Shift"   }, "j"     , function () awful.client.swap.byidx(  1)    end      , {description = "swap with next client by index", group = "client"}),
+    awful.key({ modkey, "Shift"   }, "k"     , function () awful.client.swap.byidx( -1)    end      , {description = "swap with previous client by index", group = "client"}),
+    awful.key({ modkey, "Control" }, "j"     , function () awful.screen.focus_relative( 1) end      , {description = "focus the next screen", group = "screen"}),
+    awful.key({ modkey, "Control" }, "k"     , function () awful.screen.focus_relative(-1) end      , {description = "focus the previous screen", group = "screen"}),
+    awful.key({ modkey,           }, "u"     , awful.client.urgent.jumpto                           , {description = "jump to urgent client", group = "client"}),
 
-    awful.key({ modkey,           }, "Tab"   , f_prevfocus                                                          , {description = "go back", group = "client"}),
+    awful.key({ modkey,           }, "Tab"   , f_prevfocus                                          , {description = "go back", group = "client"}),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end                                , {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Mod1"    }, "l"     , function () awful.spawn("xscreensaver-command -lock") end            , {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey,           }, "z"     , function () awful.spawn(terminal) end                                , {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r"     , awesome.restart                                                      , {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Control" }, "q"     , awesome.quit                                                         , {description = "quit awesome", group = "awesome"}),
+    awful.key({ modkey,           }, "Return", f_spawn(terminal)                                    , {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey, "Mod1"    }, "l"     , f_spawn("xscreensaver-command -lock")                , {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey,           }, "z"     , f_spawn(terminal)                                    , {description = "open a terminal", group = "launcher"}),
+    awful.key({ modkey, "Control" }, "r"     , awesome.restart                                      , {description = "reload awesome", group = "awesome"}),
+    awful.key({ modkey, "Control" }, "q"     , awesome.quit                                         , {description = "quit awesome", group = "awesome"}),
 
-    awful.key({ modkey,           }, "l"     , function () awful.tag.incmwfact( 0.05) end                           , {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkey,           }, "h"     , function () awful.tag.incmwfact(-0.05) end                           , {description = "decrease master width factor", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "h"     , function () awful.tag.incnmaster( 1, nil, true) end                  , {description = "increase the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "l"     , function () awful.tag.incnmaster(-1, nil, true) end                  , {description = "decrease the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Control" }, "h"     , function () awful.tag.incncol( 1, nil, true) end                     , {description = "increase the number of columns", group = "layout"}),
-    awful.key({ modkey, "Control" }, "l"     , function () awful.tag.incncol(-1, nil, true) end                     , {description = "decrease the number of columns", group = "layout"}),
-    awful.key({ modkey,           }, "b"     , function () awful.spawn(browser) end                                 , {description = "launch Browser", group = "launcher"}),
-    awful.key({ modkey            }, "p"     , function () awful.spawn("keepassxc") end                             , {description = "keepass", group = "launcher"}),
-    awful.key({ modkey, "Shift"   }, "space" , function () awful.layout.inc(-1) end                                 , {description = "select previous", group = "layout"}),
-    awful.key({ modkey, "Control" }, "n"     , f_restore_last_minimized                                             , {description = "restore minimized", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "n"     , f_restore_minimized_menu                                             , {description = "restore minimized menu", group = "client"}),
-    awful.key({ modkey            }, "c"     , f_jump_to_hidden_client                                              , {description = "jump to hidden client", group = "client"}),
+    awful.key({ modkey,           }, "l"     , function () awful.tag.incmwfact( 0.05) end           , {description = "increase master width factor", group = "layout"}),
+    awful.key({ modkey,           }, "h"     , function () awful.tag.incmwfact(-0.05) end           , {description = "decrease master width factor", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "h"     , function () awful.tag.incnmaster( 1, nil, true) end  , {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Shift"   }, "l"     , function () awful.tag.incnmaster(-1, nil, true) end  , {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Control" }, "h"     , function () awful.tag.incncol( 1, nil, true) end     , {description = "increase the number of columns", group = "layout"}),
+    awful.key({ modkey, "Control" }, "l"     , function () awful.tag.incncol(-1, nil, true) end     , {description = "decrease the number of columns", group = "layout"}),
+    awful.key({ modkey,           }, "b"     , function () awful.spawn(browser) end                 , {description = "launch Browser", group = "launcher"}),
+    awful.key({ modkey            }, "p"     , function () awful.spawn("keepassxc") end             , {description = "keepass", group = "launcher"}),
+    awful.key({ modkey, "Shift"   }, "space" , function () awful.layout.inc(-1) end                 , {description = "select previous", group = "layout"}),
+    awful.key({ modkey, "Control" }, "n"     , f_restore_last_minimized                             , {description = "restore minimized", group = "client"}),
+    awful.key({ modkey, "Shift"   }, "n"     , f_restore_minimized_menu                             , {description = "restore minimized menu", group = "client"}),
+    awful.key({ modkey            }, "c"     , f_jump_to_hidden_client                              , {description = "jump to hidden client", group = "client"}),
     -- Prompt
-    awful.key({ modkey            }, "r"     , f_rofi_default                                                       , {description = "run prompt", group = "launcher"}),
-    awful.key({ modkey , "Shift"  }, "r"     , f_rofi_freedesktop                                                   , {description = "run prompt", group = "launcher"}),
-    awful.key({ modkey            }, "'"     , f_lua_prompt                                                         , {description = "lua execute prompt", group = "awesome"}),
+    awful.key({ modkey            }, "r"     , f_rofi_default                                       , {description = "run prompt", group = "launcher"}),
+    awful.key({ modkey , "Shift"  }, "r"     , f_rofi_freedesktop                                   , {description = "run prompt", group = "launcher"}),
+    awful.key({ modkey            }, "'"     , f_lua_prompt                                         , {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
-    awful.key({ modkey            }, "Print" , function () awful.spawn("flameshot gui") end                         , {description = "flameshot", group = "scrot"}),
-    awful.key({ modkey , "Mod1"   }, "Print" , function () awful.spawn("sh -c \"flameshot gui --delay 3000\"") end  , {description = "flameshot with 3s delay", group = "scrot"}),
-    awful.key({ modkey , "Shift"  }, "Print" , function () awful.spawn("flameshot full --clipboard") end            , {description = "flameshot fullscreen to clipboard", group = "scrot"}),
-    awful.key({ modkey            }, "o"     , function () awful.spawn("sh -c \"sleep 3; xprop > ~/xpr\"") end      , {description = "write xprop to ~/xpr in 3 seconds", group = "scrot"}),
+    awful.key({ modkey            }, "Print" , f_spawn("flameshot gui")                             , {description = "flameshot", group = "scrot"}),
+    awful.key({ modkey , "Mod1"   }, "Print" , f_spawn("sh -c \"flameshot gui --delay 3000\"")      , {description = "flameshot with 3s delay", group = "scrot"}),
+    awful.key({ modkey , "Shift"  }, "Print" , f_spawn("flameshot full --clipboard")                , {description = "flameshot fullscreen to clipboard", group = "scrot"}),
+    awful.key({ modkey            }, "o"     , f_spawn("sh -c \"sleep 3; xprop > ~/xpr\"")          , {description = "write xprop to ~/xpr in 3 seconds", group = "scrot"}),
               
 })
 
