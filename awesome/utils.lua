7ab0863c6ad2235a9856f51071ffd03a7781f260
Mikhail Pletnev <mmp.dux@gmail.com>
2022-10-26 18:47:08 +0300

    forgot utils file

---
+++ awesome/utils.lua
@@ -0,0 +1,75 @@
+awful = require("awful")
+
+local utils = {}
+
+function utils.prevfocus()
+    awful.client.focus.history.previous()
+    if client.focus then
+        client.focus:raise()
+    end
+end
+
+function utils.restore_last_minimized()
+    c = awful.client.restore()
+    -- Focus restored client
+    if c then
+        client.focus = c
+        c:raise()
+    end
+end
+
+function utils.restore_minimized_menu()
+    awful.menu.clients (nil, nil, function(c)
+      if c.minimized and not c.hidden then
+        for _, tag in pairs(c:tags()) do
+          if tag.selected then
+            return true
+          end
+        end
+        return false
+      end
+    end)
+end
+
+function utils.jump_to_hidden_client()
+    awful.menu.clients (nil, nil, function(c)
+      if c.minimized then
+        return true
+      end
+      for _, tag in pairs(c:tags()) do
+        if tag.selected then
+          return false
+        end
+      end
+      return true
+    end)
+end
+
+function utils.rofi_default () 
+    awful.spawn("rofi -show run -matching regex -sorting-method fzf")
+end
+
+function utils.rofi_freedesktop () 
+    awful.spawn("rofi -show drun -matching regex -sorting-method fzf")
+end
+
+function utils.lua_prompt()
+    awful.prompt.run {
+      prompt       = "Run Lua code: ",
+      textbox      = awful.screen.focused().mypromptbox.widget,
+      exe_callback = awful.util.eval,
+      history_path = awful.util.get_cache_dir() .. "/history_eval"
+    }
+end
+
+function utils.spawn(cmd)
+    return function() awful.spawn(cmd) end
+end
+
+function utils.screenshot(delay)
+    if delay == 0 then
+        return utils.spawn("flameshot gui")
+    end
+    return utils.spawn('sh -c "flameshot gui --delay ' .. tostring(delay) .. '"')
+end
+return utils
