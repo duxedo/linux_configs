dc81251e3ba9e01511aa7d6d9bb719f28a1bb01c
Mikhail Pletnev <mmp.dux@gmail.com>
2023-03-07 11:12:31 +0300

    streamline bindings

--- awesome/bindings.lua
+++ awesome/bindings.lua
@@ -42,82 +42,90 @@ awful.key.keygroups["tags"] = {
     {"9", 12},
 }
 
-local key = ut.key
-local hotkeys = hotkeys_popup.new({font= "Fira Code Bold 12", description_font = "Fira Code 12", width = 2000, height = 1000,
---modifiers_fg = theme.fg_normal
-})
+local hotkeys = hotkeys_popup.new {
+    font= "Fira Code Bold 12",
+    description_font = "Fira Code 12",
+    width = 2000,
+    height = 1000,
+}
+
 vimkeys(hotkeys)
 
 local volume_widget = require('awesome-wm-widgets.volume-widget.volume')
 -- {{{ Key bindings
+local mod =  { modkey }
+local modc = { modkey, "Control" }
+local mods = { modkey, "Shift" }
+local modm = { modkey, "Mod1" }
+local modcs = { modkey, "Control", "Shift" }
 ut.register_global_bindings {
 client = {
-    {{ modkey, "Shift"   }, {"jk"}      , awful.client.swap.byidx                       , "swap with next/previous client"},
-    {{ modkey            }, {"jk"}      , ut.focus_by_idx                               , "focus next/previous by index"},
-    {{ modkey,           }, "u"         , awful.client.urgent.jumpto                    , "jump to urgent client"},
-    {{ modkey,           }, "Tab"       , ut.prev                                       , "go back"},
-    {{ modkey, "Control" }, "n"         , ut.restore_last_minimized                     , "restore minimized"},
-    {{ modkey,           }, {"-="}      , function (d) ut.inc_opacity(d * 0.1)() end    , "increase/decrease opacity"},
-    {{ modkey, "Shift"   }, "n"         , ut.restore_minimized_menu                     , "restore minimized menu"},
-    {{ modkey            }, "c"         , ut.jump_to_hidden_client                      , "jump to hidden client"},
+    { mods , {"jk"}  , awful.client.swap.byidx                  , "swap with next/previous client"},
+    { mod  , {"jk"}  , ut.focus_by_idx                          , "focus next/previous by index"},
+    { mod  , "u"     , awful.client.urgent.jumpto               , "jump to urgent client"},
+    { mod  , "Tab"   , ut.prev                                   , "go back"},
+    { modc , "n"     , ut.restore_last_minimized                , "restore minimized"},
+    { mod  , {"-="}  , function (d) ut.inc_opacity(d * 0.1)() end, "increase/decrease opacity"},
+    { mods , "n"     , ut.restore_minimized_menu                , "restore minimized menu"},
+    { mod  , "c"     , ut.jump_to_hidden_client                   , "jump to hidden client"},
 },
 media = {
-    {{}, "XF86AudioRaiseVolume"      , function() volume_widget:inc(5, true) end     , "increase volume"},
-    {{}, "XF86AudioLowerVolume"      , function() volume_widget:dec(5, true) end     , "decrease volume"},
-    {{}, "XF86AudioMute"             , function() volume_widget:toggle() end         , "mute"},
-    {{modkey}, "F10"                 , ut.toggle_autdo_profile                       , "toggle audio profile"},
+    {{}, "XF86AudioRaiseVolume", function() volume_widget:inc(5, true) end , "increase volume"},
+    {{}, "XF86AudioLowerVolume", function() volume_widget:dec(5, true) end , "decrease volume"},
+    {{}, "XF86AudioMute"       , function() volume_widget:toggle() end     , "mute"},
+    { mod, "F10"               , ut.toggle_autdo_profile                   , "toggle audio profile"},
 },
 notifications = {
-    {{modkey }, "v"                  , ut.spawn("dunstctl close-all")                , "close all"},
-    {{modkey, "Control" }, "v"       , ut.spawn("dunstctl history-pop")              , "hitory"},
-    {{modkey, "Shift" }, "v"         , ut.spawn("dunstctl context")                  , "context menu"},
+    { mod,  "v"     , ut.spawn("dunstctl close-all")        , "close all"},
+    { modc, "v"     , ut.spawn("dunstctl history-pop")      , "hitory"},
+    { mods, "v"     , ut.spawn("dunstctl context")          , "context menu"},
 },
 tag = {
-    {{ modkey }, "Escape"            , function() awful.tag.history.restore(nil, 1) end   , "go back"},
+    { mod, "Escape" , function() awful.tag.history.restore(nil, 1) end   , "go back"},
 },
 screen = {
-    {{ modkey, "Control" }, {"jk"}      , awful.screen.focus_relative                , "focus the next/previous screen"},
+    { modc,  {"jk"}  , awful.screen.focus_relative                , "focus the next/previous screen"},
 },
 awesome = {
-    {{ modkey }           , "s"      , function() hotkeys:show_help() end            , "show help"},
-    {{ modkey, "Shift"   }, "b"      , ut.toggle_wibar                                  , "hide wibar"},
-    {{ modkey            }, "'"      , ut.lua_prompt                                 , "lua execute prompt"},
-    {{ modkey, "Control" }, "r"      , awesome.restart                               , "reload awesome"},
-    {{ modkey, "Control" }, "x"      , awesome.quit                                  , "quit awesome"},
+    { mod,  "s"      , function() hotkeys:show_help() end            , "show help"},
+    { mods, "b"      , ut.toggle_wibar                                  , "hide wibar"},
+    { mod,  "'"      , ut.lua_prompt                                 , "lua execute prompt"},
+    { modc, "r"      , awesome.restart                               , "reload awesome"},
+    { modc, "x"      , awesome.quit                                  , "quit awesome"},
 },
 launcher = {
-    {{ modkey,           }, "Return", ut.spawn(const.terminal)                              , "open a terminal"},
-    {{ modkey, "Mod1"    }, "l"     , ut.spawn("xscreensaver-command -lock")                , "lock station"},
-    {{ modkey,           }, "a"     , ut.spawn(const.terminal)                              , "open a terminal"},
-    {{ modkey,           }, "b"     , ut.spawn(const.browser)                               , "launch Browser"},
-    {{ modkey            }, "p"     , ut.raise_or_spawn("keepassxc")                                 , "keepass"},
-    {{ modkey            }, "r"     , ut.rofi(), "run prompt"},
-    {{ modkey            }, "g"     , ut.rofi({show = "drun", ['drun-categories'] = "Game"}), "run games"},
-    {{ modkey , "Shift"  }, "r"     , ut.rofi({show = "drun"}), "run desktop apps"},
-    {{ modkey            }, "d"     , ut.rofi(nil, {floating = true}), "run prompt floating"},
-    {{ modkey , "Shift"  }, "d"     , ut.rofi({show = "drun"}, {floating = true}), "run desktop apps floating"},
+    { mod , "Return" , ut.spawn(const.terminal)                              , "open a terminal"},
+    { modm, "l"      , ut.spawn("xscreensaver-command -lock")                , "lock station"},
+    { mod , "a"      , ut.spawn(const.terminal)                              , "open a terminal"},
+    { mod , "b"      , ut.spawn(const.browser)                               , "launch Browser"},
+    { mod,  "p"      , ut.raise_or_spawn("keepassxc")                                 , "keepass"},
+    { mod,  "r"      , ut.rofi(), "run prompt"},
+    { mod,  "g"      , ut.rofi({show = "drun", ['drun-categories'] = "Game"}), "run games"},
+    { mods, "r"      , ut.rofi({show = "drun"}), "run desktop apps"},
+    { mod,  "d"      , ut.rofi(nil, {floating = true}), "run prompt floating"},
+    { mods, "d"      , ut.rofi({show = "drun"}, {floating = true}), "run desktop apps floating"},
 },
 utility = {
     {{ modkey, "Mod1"    }, "s"     , ut.spawn("sync")                                      , "call fsync"},
 },
 layout = {
-    {{ modkey            }, {"hl"}  , function (d) awful.tag.incmwfact( d * 0.005) end      , "inc/dec master width factor"},
-    {{ modkey, "Shift"   }, {"hl"}  , function (d) awful.tag.incnmaster( d, nil, true) end  , "inc/dec # of master clients"},
-    {{ modkey, "Control" }, {"hl"}  , function (d) awful.tag.incncol( d, nil, true) end     , "inc/dec # of columns"},
-    {{ modkey, "Shift"   }, {",."}  , awful.layout.inc                                      , "previous/next"},
+    { mod,   {"hl"}, function (d) awful.tag.incmwfact( d * 0.005) end      , "inc/dec master width factor"},
+    { mods,  {"hl"}, function (d) awful.tag.incnmaster( d, nil, true) end  , "inc/dec # of master clients"},
+    { modc,  {"hl"}, function (d) awful.tag.incncol( d, nil, true) end     , "inc/dec # of columns"},
+    { mods,  {",."}, awful.layout.inc                                      , "previous/next"},
 },
 scrot = {
-    {{ modkey            }, "Print" , ut.screenshot(0)                                      , "flameshot"},
-    {{ modkey , "Mod1"   }, "Print" , ut.screenshot(3000)                                   , "flameshot with 3s delay"},
-    {{ modkey , "Shift"  }, "Print" , ut.spawn("flameshot full --clipboard")                , "flameshot fullscreen to clipboard"},
-    {{ modkey            }, "o"     , ut.spawn("sh -c \"sleep 3; xprop > ~/xpr\"")          , "write xprop to ~/xpr in 3 seconds"},
+    { mod,  "Print", ut.screenshot(0)                                      , "flameshot"},
+    { modm, "Print", ut.screenshot(3000)                                   , "flameshot with 3s delay"},
+    { mods, "Print", ut.spawn("flameshot full --clipboard")                , "flameshot fullscreen to clipboard"},
+    { mod,  "o"    , ut.spawn("sh -c \"sleep 3; xprop > ~/xpr\"")          , "write xprop to ~/xpr in 3 seconds"},
 },
 tags = {
-    {{ modkey }, {"tags"}, ut.view_tag, "view tag" },
-    {{ modkey, "Control" }, {"tags"}, ut.toggle_tag, "toggle tag" },
-    {{ modkey, "Shift" }, {"tags"}, ut.move_to_tag, "move to tag" },
-    {{ modkey, "Mod1" }, {"tags"}, ut.move_and_switch, "move + swtich to tag" },
-    {{ modkey, "Control", "Shift" }, {"tags"}, ut.toggle_client_on_tag, "toggle client on tag"},
+    { mod,   {"tags"}, ut.view_tag, "view tag" },
+    { modc,  {"tags"}, ut.toggle_tag, "toggle tag" },
+    { mods,  {"tags"}, ut.move_to_tag, "move to tag" },
+    { modm,  {"tags"}, ut.move_and_switch, "move + swtich to tag" },
+    { modcs, {"tags"}, ut.toggle_client_on_tag, "toggle client on tag"},
 }
 }
 
@@ -125,26 +133,26 @@ tags = {
 client.connect_signal("request::default_keybindings", function(context)
     ut.register_client_bindings{
         client = {
-            {{ modkey,           }, "f", ut.fullscreen, "toggle fullscreen"},
-            {{ modkey            }, "x",      function (c) awful.client.focus.history.previous() c:kill() end, "close"},
-            {{ modkey, "Control" }, "space",  awful.client.floating.toggle, "toggle floating"},
-            {{ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end, "move to master"},
-            {{ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end, "toggle keep on top"},
-            {{ modkey,           }, "n", function (c) c.minimized = true end , "minimize"},
-            {{ modkey,           }, {"[]"}, function (d, c) awful.client.incwfact(0.05 * d, c) end , "dec/inc size factor"},
-            {{ modkey,           }, "m", ut.maximize(), "(un)maximize"},
-            {{ modkey, "Control" }, "m", ut.maximize(ut.vert), "(un)maximize vertically"},
-            {{ modkey, "Shift"   }, "m", ut.maximize(ut.hor), "(un)maximize horizontally"},
-            {{ modkey, "Control" }, "d", ut.dumpclient, "dump client properties"},
-            {{ modkey            }, {"arrows"}, ut.resize, "resize client"},
-            {{ modkey, "Shift"   }, {"arrows"}, ut.move, "move client"},
+            { mod , "f", ut.fullscreen, "toggle fullscreen"},
+            { mod  , "x",      function (c) awful.client.focus.history.previous() c:kill() end, "close"},
+            { modc ,  "space",  awful.client.floating.toggle, "toggle floating"},
+            { modc,  "Return", function (c) c:swap(awful.client.getmaster()) end, "move to master"},
+            { mod , "t",      function (c) c.ontop = not c.ontop            end, "toggle keep on top"},
+            { mod , "n", function (c) c.minimized = true end , "minimize"},
+            { mod , {"[]"}, function (d, c) awful.client.incwfact(0.05 * d, c) end , "dec/inc size factor"},
+            { mod , "m", ut.maximize(), "(un)maximize"},
+            { modc,  "m", ut.maximize(ut.vert), "(un)maximize vertically"},
+            { mods,  "m", ut.maximize(ut.hor), "(un)maximize horizontally"},
+            { modc,  "d", ut.dumpclient, "dump client properties"},
+            { mod, {"arrows"}, ut.resize, "resize client"},
+            { mods,  {"arrows"}, ut.move, "move client"},
         }
     }
 end)
 
 --awful.keyboard.append_global_keybindings({
     --awful.key {
-    --    modifiers   = { modkey },
+    --    modifiers   =  mod,
     --    keygroup    = "numpad",
     --    description = "select layout directly",
     --    group       = "layout",
@@ -162,10 +170,10 @@ client.connect_signal("request::default_mousebindings", function()
         awful.button({ }, 1, function (c)
             c:activate { context = "mouse_click" }
         end),
-        awful.button({ modkey }, 1, function (c)
+        awful.button( mod, 1, function (c)
             c:activate { context = "mouse_click", action = "mouse_move"  }
         end),
-        awful.button({ modkey }, 3, function (c)
+        awful.button( mod, 3, function (c)
             c:activate { context = "mouse_click", action = "mouse_resize"}
         end),
     }
@@ -185,7 +193,7 @@ end)
 --    },
 --    stop_event         = 'release',
 --    root_keybindings = {
---        {{modkey}, 'z', function(self)
+--        { mod, 'z', function(self)
 --            print('Is now active!', self)
 --        end},
 --    },
