3d00aaf1bc541b9e23f9ce29aca567dee92adda7
Mikhail Pletnev <mmp.dux@gmail.com>
2024-07-18 13:56:55 +0300

    chore: prettify taglist bindings

--- awesome/mywibox.lua
+++ awesome/mywibox.lua
@@ -36,23 +36,20 @@ local function client_menu_toggle_fn()
         end
     end
 end
+local function iffocussed(f)
+    return function (t)
+        if client.focus then
+            f(client.focus, t)
+        end
+    end
+end
 -- Create a wibox for each screen and add it
 local taglist_buttons =
     gears.table.join(
         awful.button({}, 1, function(t) t:view_only() end),
-        awful.button({ modkey }, 1,
-            function(t)
-                if client.focus then
-                    client.focus:move_to_tag(t)
-                end
-            end),
+        awful.button({ modkey }, 1, iffocussed(awful.client.object.move_to_tag)),
         awful.button({}, 3, awful.tag.viewtoggle),
-        awful.button({ modkey }, 3,
-            function(t)
-                if client.focus then
-                    client.focus:toggle_tag(t)
-                end
-            end),
+        awful.button({ modkey }, 3, iffocussed(awful.client.object.toggle_tag)),
         awful.button({}, 4, function(t) awful.tag.viewnext(t.screen) end),
         awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
     )
